ccc3bd1114dfda1222374d22c9e87b3acd7b85e3
Cassio Basile <cassio.basile@deltamind.com.br>
2025-12-11 08:26:35 -0300

    Add ReplaceLastOccurrence string extension method
    Added ReplaceLastOccurrence to StringExtensions, allowing replacement of the last occurrence of a substring. Note: method currently does not return the modified string due to void return type and string immutability.

--- BEA.Extensions/StringExtensions.cs
+++ BEA.Extensions/StringExtensions.cs
@@ -1,11 +1,22 @@
-﻿using System;
-
-namespace Emobi.Extensions;
+﻿namespace Emobi.Extensions;
 
 public static class StringExtensions
 {
     public static T IfNotNullOrEmpty<T>(this string value, Func<string, T> func, T defaultValue = default)
     {
         return string.IsNullOrEmpty(value) ? defaultValue : func(value);
+    }
+
+    public static void ReplaceLastOccurrence(this string value, string find, string replace)
+    {
+        if (string.IsNullOrEmpty(value) || string.IsNullOrEmpty(find))
+            return;
+
+        int place = value.LastIndexOf(find, StringComparison.Ordinal);
+
+        if (place == -1)
+            return;
+
+        value = value.Remove(place, find.Length).Insert(place, replace);
     }
 }
