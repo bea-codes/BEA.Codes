e2e19fab43bd409d3b6d9bd1ef76e37261516f04
Cassio Basile <cassio.basile@deltamind.com.br>
2025-12-11 07:07:07 -0300

    Add project files.

---
+++ BEA.Extensions/DataTableExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+
+namespace Emobi
+{
+    public static class DataTableExtensions
+    {
+        public static DataTable ToDataTable<T>(this IList<T> data)
+        {
+            var props = TypeDescriptor.GetProperties(typeof(T));
+            var table = new DataTable();
+
+            for (int i = 0, loopTo = props.Count - 1; i <= loopTo; i++)
+            {
+                var prop = props[i];
+                table.Columns.Add(prop.Name, prop.PropertyType);
+            }
+
+            object[] values = new object[props.Count];
+
+            foreach (T item in data)
+            {
+
+                for (int i = 0, loopTo1 = values.Length - 1; i <= loopTo1; i++)
+                    values[i] = props[i].GetValue(item);
+
+                table.Rows.Add(values);
+            }
+
+            return table;
+        }
+
+        public static T Then<T>(this bool value, T result)
+        {
+            return value ? result : default(T);
+        }
+    }
+}
---
+++ BEA.Extensions/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Emobi.Extensions;
+
+public static class StringExtensions
+{
+    public static T IfNotNullOrEmpty<T>(this string value, Func<string, T> func, T defaultValue = default)
+    {
+        return string.IsNullOrEmpty(value) ? defaultValue : func(value);
+    }
+}
